09673753af7a8b16181f29ccfd306efc74c0b507
Archie Newbold <archienewbold@aol.com>
2023-10-16 12:09:26 +0100

    added sqlqueries file adding two fields to colour and selecting

---
+++ SQLqueries.sql
@@ -0,0 +1,13 @@
+use monday; -- 'enter' the monday database, all lines below executed in monday
+
+
+select * from colour;    -- select all from table colour
+ 
+insert into colour(colour_name) -- add data (values) into colour table in colour_name field 
+values ('Purple');
+
+insert into colour(colour_name) -- add data (values) into colour table in colour_name field 
+values ('Blue');
+
+
+select * from person;	  -- select all from table person	
