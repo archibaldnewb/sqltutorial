a47b656c4abee01e5a2cb7e09b1c2eb77f370117
Archie Newbold <archienewbold@aol.com>
2023-10-16 11:57:05 +0100

    changed order of table creation

--- sqltutorial.sql
+++ sqltutorial.sql
@@ -2,6 +2,12 @@
 
 use monday; -- 'enter' the monday database, all lines below executed in monday
 
+create table colour
+(
+id int not null primary key auto_increment, -- adds mandatory primary key 'id' int field with auto increment
+colour_name varchar(25) not null -- add mandatory colour_name field
+);
+
 create table person -- create table 'person' abiding to following parameters
 (
 id int not null primary key auto_increment, -- adds mandatory primary key 'id' int field with auto increment
@@ -12,8 +18,4 @@ colour_id int, -- add colour_id field (not mandatory)
 foreign key (colour_id) references colour(id)    -- add foreign key for colour_id 'id' from colour table 
 );
 
-create table colour
-(
-id int not null primary key auto_increment, -- adds mandatory primary key 'id' int field with auto increment
-colour_name varchar(25) not null -- add mandatory colour_name field
-);
+
